4c70588a4546f466151a06d8bcf18a1d38ff6fb0
Dorian Karter <dkarter@gmail.com>
2016-04-01 09:48:30 -0500

    Fix indentation

--- src/index.js
+++ src/index.js
@@ -12,5 +12,6 @@ const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <App />
-  </Provider>
-  , document.querySelector('.container'));
+  </Provider>,
+  document.querySelector('.container')
+);
